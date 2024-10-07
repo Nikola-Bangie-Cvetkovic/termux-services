2be7e9ee50c4225b0828387829079ea2c49028c8
Nikola Bangie Cvetkovic <icyz.47@gmail.com>
2024-10-07 15:18:18 +0200

    add local ip readout

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+require("dotenv").config();
+const os = require("os");
 const express = require("express");
 const server = express();
 
@@ -10,7 +11,11 @@ server.use(express.json());
 
 server.use("/sms", smsRoutes);
 
+const getLocalIPAddress = () =>
+    Object.values(os.networkInterfaces()).flat().find(iface => iface.family === "IPv4" && !iface.internal)?.address || "localhost";
 
 server.listen(port, () => {
     console.info(`Listening on port ${port}`);
+    console.info(`Local ip address: ${getLocalIPAddress()}`);
+
 });
